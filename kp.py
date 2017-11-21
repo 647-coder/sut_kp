04a0c4a434589764e300d185e9c26fa8da123afd
647 <liusiqi@ustil.cn>
2017-11-21 14:46:53 +0800

    Fix bug

--- kp.py
+++ kp.py
@@ -1,4 +1,4 @@
-#coding: utf-8
+# coding: utf-8
 import requests
 import re
 import sys
@@ -8,15 +8,14 @@ from PIL import Image
 
 print("欢迎使用沈阳工业大学自动教评系统。Github: github.com/647-coder/sut_kp。")
 
-header = {  "Accept":"text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
-            "Accept-Encoding":"gzip, deflate",
-            "Accept-Language":"zh-CN,zh;q=0.8",
-            "Cache-Control":"max-age=0",
-            "Connection":"keep-alive",
-            "Content-Type":"application/x-www-form-urlencoded",
-            "Upgrade-Insecure-Requests":"1",
-            "User-Agent":"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36",
-            }
+header = {"Accept":"text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+          "Accept-Encoding":"gzip, deflate",
+          "Accept-Language":"zh-CN,zh;q=0.8",
+          "Cache-Control":"max-age=0",
+          "Connection":"keep-alive",
+          "Content-Type":"application/x-www-form-urlencoded",
+          "Upgrade-Insecure-Requests":"1",
+          "User-Agent":"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36"}
 
 username = None
 passowrd = None
@@ -31,7 +30,7 @@ else:
 print("正在登录...账号：{0} 密码：{1}".format(username, password))
 
 session = requests.Session()
-s.headers.update(header)
+session.headers.update(header)
 session.get("http://jwc.sut.edu.cn/")
 
 tryflag = 1
@@ -45,17 +44,17 @@ while True:
     try:
         import pytesseract
         vcode = pytesseract.image_to_string(im)
-    except:
+    except ImportError:
         im.show()
         vcode = input('验证码 > ')
-    loginparams = { 'WebUserNO' : username,
-                    'Password' : password,
-                    'Agnomen' : vcode,
-                    'submit.x': '30' ,
-                    'submit.y' : '20'
-                    }
+    loginparams = {'WebUserNO': username,
+                   'Password': password,
+                   'Agnomen': vcode,
+                   'submit.x': '30',
+                   'submit.y': '20'}
 
-    login = session.post("http://jwc.sut.edu.cn/ACTIONLOGON.APPPROCESS", data=loginparams)
+    login = session.post("http://jwc.sut.edu.cn/ACTIONLOGON.APPPROCESS",
+                         data=loginparams)
     loginpage = login.text
 
     if u"个人信息" in loginpage:
@@ -64,8 +63,6 @@ while True:
     elif u"错误的" in loginpage:
         print("用户名或密码错误。")
         os._exit(1)
-        #username = input('用户名 > ')
-        #password = input('密码 > ')
     else:
         print("自动识别验证码错误，正在重新尝试...")
     if tryflag >= 3:
@@ -107,27 +104,27 @@ for each in courses:
     com5 = re.compile(reg6)
     com6 = re.compile(reg7)
     com7 = re.compile(reg2)
-    params = {  'ItemTypeScore1': "98",
-                'ItemTypeID': "1",
-                'SeriesID' : str(re.findall(com1, page)[0]),
-                'PaperID' : str(re.findall(com2, page)[0]),
-                'TaskID' : str(re.findall(com3, page)[0]),
-                'TeacherNO': str(re.findall(com4, page)[0]),
-                'adjustCode' : str(re.findall(com7, page)[0]),
-                'PaperScore' : "98",
-                'ResultMemo' : "",
-                'Score1_1': '100',
-                'Score1_2': '100',
-                'Score1_3': '100',
-                'Score1_4': '100',
-                'Score1_5': '100',
-                'Score1_6': '80',
-                'Score1_7': '100',
-                'ResultID': ['1_1','1_2','1_3','1_4','1_5','1_6','1_7']
-                }
+    params = {'ItemTypeScore1': "98",
+              'ItemTypeID': "1",
+              'SeriesID': str(re.findall(com1, page)[0]),
+              'PaperID': str(re.findall(com2, page)[0]),
+              'TaskID': str(re.findall(com3, page)[0]),
+              'TeacherNO': str(re.findall(com4, page)[0]),
+              'adjustCode': str(re.findall(com7, page)[0]),
+              'PaperScore': "98",
+              'ResultMemo': "",
+              'Score1_1': '100',
+              'Score1_2': '100',
+              'Score1_3': '100',
+              'Score1_4': '100',
+              'Score1_5': '100',
+              'Score1_6': '80',
+              'Score1_7': '100',
+              'ResultID': ['1_1', '1_2', '1_3', '1_4', '1_5', '1_6', '1_7']}
     print("课评进度({0}/{1})...".format(i, len(courses)))
     i += 1
-    session.post("http://jwc.sut.edu.cn/ACTIONJSUPDATECONTENTRESULT.APPPROCESS", data=params)
+    session.post("http://jwc.sut.edu.cn/ACTIONJSUPDATECONTENTRESULT.APPPROCESS",
+                 data=params)
 
 print("已全部完成！")
 
